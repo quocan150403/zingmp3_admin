1e30b4b22ab2842d37faad4766bef764dc66c5f3
Anvq2003 <97084643+Anvq2003@users.noreply.github.com>
2023-08-20 14:56:42 +0700

    Add logo

--- src/layouts/dashboard/nav/index.js
+++ src/layouts/dashboard/nav/index.js
@@ -64,7 +64,7 @@ export default function Nav({ openNav, onCloseNav }) {
 
             <Box sx={{ ml: 2 }}>
               <Typography variant="subtitle2" sx={{ color: 'text.primary' }}>
-                {account.displayName}
+                Quốc An
               </Typography>
 
               <Typography variant="body2" sx={{ color: 'text.secondary' }}>
